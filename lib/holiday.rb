44894e80f02e10b1e0c842b82df35899c4b49cff
Trevor Russell <truss.42@gmail.com>
2018-06-14 20:54:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts season.to_s.capitalize << ":"
     holidays.each do |holiday, supplies|
       if holiday.to_s.include?("_")
-      puts "  #{holiday.to_s.split("_").capitalize.join(" ")}: #{supplies.join(", ")}"
+      puts "  #{holiday.to_s.split("_").each {|word| word.capitalize}.join(" ")}: #{supplies.join(", ")}"
     else
       puts "  #{holiday.to_s.capitalize}: #{supplies.join(", ")}"
     end
