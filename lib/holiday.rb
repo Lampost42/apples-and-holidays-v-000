fa53c775d0c110b563ebb0d4a8e18c688c38e952
Trevor Russell <truss.42@gmail.com>
2018-06-14 20:56:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ def all_supplies_in_holidays(holiday_hash)
     holidays.each do |holiday, supplies|
       puts "  #{holiday.to_s.split("_").each {|word| word.capitalize}.join(" ")}: #{supplies.join(", ")}"
     end
-    end
+
   end
 
 end
